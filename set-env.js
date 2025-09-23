b07a96f23d28e50efa2260175d367a8f9bac6713
MLheriteau <michel.lheriteau@hotmail.fr>
2025-09-23 15:05:07 +0200

    test

--- set-env.js
+++ set-env.js
@@ -1,18 +1,18 @@
-import * as fs from 'fs';
-import * as dotenv from 'dotenv';
+// import * as fs from 'fs';
+// import * as dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const targetPath = './src/environments/environment.ts';
+// const targetPath = './src/environments/environment.ts';
 
-const envConfigFile = `
-export const environment = {
-  production: ${process.env.NODE_ENV === 'production'},
-  emailJsServiceId: '${process.env.EMAILJS_SERVICE_ID || ''}',
-  emailJsTemplateId: '${process.env.EMAILJS_TEMPLATE_ID || ''}',
-  emailJsPublicKey: '${process.env.EMAILJS_PUBLIC_KEY || ''}'
-};
-`;
+// const envConfigFile = `
+// export const environment = {
+//   production: ${process.env.NODE_ENV === 'production'},
+//   emailJsServiceId: '${process.env.EMAILJS_SERVICE_ID || ''}',
+//   emailJsTemplateId: '${process.env.EMAILJS_TEMPLATE_ID || ''}',
+//   emailJsPublicKey: '${process.env.EMAILJS_PUBLIC_KEY || ''}'
+// };
+// `;
 
-fs.writeFileSync(targetPath, envConfigFile, { encoding: 'utf-8' });
-console.log('✅ environment.ts généré avec succès');
+// fs.writeFileSync(targetPath, envConfigFile, { encoding: 'utf-8' });
+// console.log('✅ environment.ts généré avec succès');
