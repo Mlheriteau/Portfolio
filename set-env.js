6c3461b4cc1b3879e280ff376b71d8e0bcfa332f
MLheriteau <michel.lheriteau@hotmail.fr>
2025-09-23 13:30:12 +0200

    correction form

--- set-env.js
+++ set-env.js
@@ -3,16 +3,16 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const targetPath = './src/environments/environment.prod.ts';
+const targetPath = './src/environments/environment.ts';
 
 const envConfigFile = `
 export const environment = {
-  production: true,
-  emailJsServiceId: '${process.env.EMAILJS_SERVICE_ID}',
-  emailJsTemplateId: '${process.env.EMAILJS_TEMPLATE_ID}',
-  emailJsPublicKey: '${process.env.EMAILJS_PUBLIC_KEY}'
+  production: ${process.env.NODE_ENV === 'production'},
+  emailJsServiceId: '${process.env.EMAILJS_SERVICE_ID || ''}',
+  emailJsTemplateId: '${process.env.EMAILJS_TEMPLATE_ID || ''}',
+  emailJsPublicKey: '${process.env.EMAILJS_PUBLIC_KEY || ''}'
 };
 `;
 
 fs.writeFileSync(targetPath, envConfigFile, { encoding: 'utf-8' });
-console.log('✅ environment.prod.ts généré avec succès');
+console.log('✅ environment.ts généré avec succès');
